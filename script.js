04c37f2549810248ab444c18dde505dc36d09761
ozod-0221 <118391973+ozod-0221@users.noreply.github.com>
2025-03-16 01:46:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let tg = window.Telegram.WebApp; // Telegram API ni chaqiramiz
+
+tg.expand(); // Ekranni to'liq qilib ochish
+
+document.getElementById("user-name").textContent = tg.initDataUnsafe?.user?.first_name || "Noma’lum";
+
+document.getElementById("send-data").addEventListener("click", function() {
+    tg.sendData("Foydalanuvchi tugmani bosdi!"); // Botga ma'lumot yuborish
+});
